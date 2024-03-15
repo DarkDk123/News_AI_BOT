2dd1654a3b660e481c462ffef257405c8c490a29
DarkLord <dipeshrathore122@gmail.com>
2024-03-15 21:22:41 +0530

    NewsDataAPI class

--- NewsFetchClasses/Fetch_news.py
+++ NewsFetchClasses/Fetch_news.py
@@ -1,5 +1,10 @@
 from newsapi import NewsApiClient
+from newsdataapi import NewsDataApiClient
+from dotenv import load_dotenv
 import os
+
+# Load the API Keys from .env file
+load_dotenv()
 
 
 # NewsAPI wrapper Inheriting NewsApiClient
@@ -12,3 +17,9 @@ class NewsAPI(NewsApiClient):
         super().__init__(os.environ.get("NEWSAPI_KEY"))
 
 
+# NewsData.io wrapper
+# 200 Credits/day | 10 Articles/credit
+
+class NewsDataAPI(NewsDataApiClient):
+    def __init__(self) -> None:
+        super().__init__(apikey=os.environ.get("NEWSDATA_KEY"))
