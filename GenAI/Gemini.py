8237dc32a505db99f49e7b13d7ee9b2b87a85b33
DarkLord <dipeshrathore122@gmail.com>
2024-03-18 22:03:43 +0530

    Tested Gemini with Telegram Bot with AsyncIO : It's Quick

--- GenAI/Gemini.py
+++ GenAI/Gemini.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv  # To load .evn file
 import os
 
 # ---------------Other_Imports----------------------------------
-from google.generativeai.types import generation_types
+from google.generativeai import types
 
 
 # Load the API Keys from .env file
 load_dotenv()
 
 # Generation Configuration for Gemini-Pro
-generation_config = {
+generation_config:dict[str, int|float] = {
     "temperature": 0.9,
     "top_p": 1,
     "top_k": 1,
@@ -23,16 +23,40 @@ class GeminiAI:
     def __init__(self, model: str = "gemini-pro") -> None:
         # Configure the model
         genai.configure(
-            api_key=os.getenv("GEMINI_KEY"),
+            api_key=os.getenv(key="GEMINI_KEY"),
         )
 
         # Model creation
-        self.model = genai.GenerativeModel(model, generation_config=generation_config)
+        self.model = genai.GenerativeModel(model_name=model, generation_config=generation_config)
+        self.chat : genai.ChatSession = None
 
-    def generate_text(
+    async def generate_text_async(
         self,
         input_: str,
     ) -> str:
-        return self.model.generate_content(input_).text
-    
+        async_response: types.AsyncGenerateContentResponse = await self.model.generate_content_async(contents=input_)
+        return async_response.text
 
+    def _create_chat(self) -> None:
+        self.chat = self.model.start_chat()
+
+    async def generate_text_async_chat(self, input_: str) -> str:
+        """Generates text asynchronously using a chat instance.
+
+        If a chat instance does not exist, a new one is created.
+
+        Args:
+            `input_:` The input text to generate a response for.
+
+        Returns:
+            The generated text.
+        """
+
+        if not self.chat:
+            self._create_chat()
+
+        async_response: types.AsyncGenerateContentResponse = (
+            await self.chat.send_message_async(content=input_)
+        )
+
+        return async_response.text
--- TelegramBot/TeleBot.py
+++ TelegramBot/TeleBot.py
@@ -8,13 +8,14 @@ from aiogram import Bot, Dispatcher, Router, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.utils import markdown
+from aiogram import exceptions as aio_exceptions
 
 sys.path.append("../")
 from GenAI.Gemini import GeminiAI
 
 # Bot token can be obtained via https://t.me/BotFather
 load_dotenv()
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN")
 
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
@@ -29,7 +30,8 @@ async def command_start_handler(message: types.Message) -> None:
     """
     This handler receives messages with `/start` command
     """
-    await message.answer(f"Hello, {markdown.hbold(message.from_user.full_name)}!")
+    await message.answer(text=f"Hello, {markdown.hbold(message.from_user.full_name)}!")
+
 
 @dp.message()
 async def AI_response(message: types.Message) -> None:
@@ -39,28 +41,30 @@ async def AI_response(message: types.Message) -> None:
     Args:
         message: The message to respond to.
     """
-     
+
     try:
         # Send a copy of the received message
         await message.answer(
-            AI.generate_text(message.text),
+            text=await AI.generate_text_async_chat(input_=message.text),
             parse_mode="markdown",
         )
     except ValueError:
         # If AI returned None!
-        await message.answer("Please try again with other input!")
+        await message.answer(text="Please try again with other input!")
     except TypeError:
         # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+        await message.answer(text="Nice try!")
+    except aio_exceptions.TelegramBadRequest:
+        await message.answer(text="Try Again!!")
 
 
 async def main() -> None:
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
     # And the run events dispatching
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+    asyncio.run(main=main())
