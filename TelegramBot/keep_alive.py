0c3cdcd3150217bc4fe4a4f8a14c53a163a2370e
DarkLord <dipeshrathore122@gmail.com>
2024-05-13 13:38:20 +0530

    Enhanced Bot Status page with additional details.
    * Modified `keep_alive.py`

--- TelegramBot/keep_alive.py
+++ TelegramBot/keep_alive.py
@@ -8,13 +8,19 @@ This file contains a `FastAPI` Application, it is used to utilize
 from fastapi import FastAPI
 import uvicorn
 from threading import Thread
+from config.settings import BOT_USER, GITHUB_URL
 
 app = FastAPI()
 
 
 @app.get("/")
 async def index():
-    return {"message": "Alive"}
+    return {
+        "STATUS": "Alive",
+        "Bot_URL_Desktop": f"https://web.telegram.org/k/#{BOT_USER}",
+        "Bot_URL_Mobile": f"https://t.me/{BOT_USER[1:]}",
+        "GITHUB_URL": GITHUB_URL,
+    }
 
 
 # Function to run the server using Uvicorn programmatically
